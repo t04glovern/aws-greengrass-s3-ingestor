bf70d37b46a0afdb4e347f9c6cbd477198838875
Nathan Glover <nathan@glovers.id.au>
2023-09-16 11:07:45 +0000

    feat: better StreamManager exception handling

--- src/DirectoryUploader.py
+++ src/DirectoryUploader.py
@@ -28,6 +28,14 @@ from stream_manager.util import Util
 
 @dataclass
 class UploaderConfig:
+    """Configuration for DirectoryUploader.
+
+    Attributes:
+        bucket_name (str): The name of the S3 bucket where files will be uploaded.
+        prefix (str): The prefix for the S3 keys.
+        interval (int): The time interval (in seconds) to wait between scanning the directory for new files.
+        path (str): The directory path to monitor for new files.
+    """
     bucket_name: str
     prefix: str
     interval: int
@@ -35,7 +43,7 @@ class UploaderConfig:
 
 
 class DirectoryUploader:
-    """DirectoryUploader monitors a folder for new files and upload those new files to S3 via stream manager."""
+    """Monitors a folder for new files and uploads those new files to S3 via stream manager."""
 
     def __init__(
         self,
@@ -43,7 +51,14 @@ class DirectoryUploader:
         logger: logging.Logger,
         client: StreamManagerClient = None,
     ):
-        """Initialize DirectoryUploader."""
+        """Initializes DirectoryUploader.
+
+        Args:
+            config (UploaderConfig): Configuration parameters for DirectoryUploader.
+            logger (logging.Logger): Logger instance for logging messages and exceptions.
+            client (StreamManagerClient, optional): Client to manage the message stream. 
+                If not provided, a default StreamManagerClient instance will be created.
+        """
 
         # Configuration parameters
         self.pathname = config.path
@@ -71,22 +86,22 @@ class DirectoryUploader:
         self._create_streams()
 
     def _delete_existing_streams(self):
-        """Delete existing streams if they exist."""
+        """Deletes the existing streams if they exist."""
 
-        # Delete the status stream for a fresh start.
-        try:
-            self.client.delete_message_stream(stream_name=self.status_stream_name)
-        except ResourceNotFoundException:
-            pass
-
-        # Delete the main stream for a fresh start.
-        try:
-            self.client.delete_message_stream(stream_name=self.stream_name)
-        except ResourceNotFoundException:
-            pass
+        for stream in [self.status_stream_name, self.stream_name]:
+            try:
+                self.client.delete_message_stream(stream_name=stream)
+            except ResourceNotFoundException:
+                pass
+            except StreamManagerException as e:
+                self.logger.error(f"Error deleting stream {stream}: {e}")
+            except ConnectionError:
+                self.logger.error(f"Connection error when trying to delete {stream}. Retrying later.")
+            except asyncio.TimeoutError:
+                self.logger.warning(f"Request to delete {stream} timed out. Retrying later.")
 
     def _create_streams(self):
-        """Create new streams for the current session."""
+        """Creates new streams for the current session."""
 
         # Prepare an export definition.
         exports = ExportDefinition(
@@ -119,7 +134,12 @@ class DirectoryUploader:
         )
 
     async def _scan(self, under_test: bool=False):
-        """Scan the directory for new files and upload them."""
+        """Scans the directory for new files and uploads them.
+
+        Args:
+            under_test (bool, optional): Flag to determine if the function is 
+                being executed under a test environment. Defaults to False.
+        """
 
         keep_looping = True
         while keep_looping:
@@ -159,12 +179,25 @@ class DirectoryUploader:
                     )
                     if not under_test:
                         await asyncio.sleep(60)
-            except Exception:
-                self.logger.exception("Exception while scanning directory")
+            except StreamManagerException as e:
+                self.logger.error(f"StreamManagerException occurred while scanning: {e}")
+            except ConnectionError:
+                self.logger.error("Connection error while scanning. Retrying in a few seconds...")
+                await asyncio.sleep(5)
+            except asyncio.TimeoutError:
+                self.logger.warning("Scanning request to StreamManager timed out. Retrying...")
+                await asyncio.sleep(1)
+            except Exception as e:
+                self.logger.exception(f"Unexpected error while scanning: {e}")
+                await asyncio.sleep(5)
             keep_looping = not under_test
 
     async def _append_s3_task(self, file: str):
-        """Append a S3 Task definition to the stream and log the sequence number."""
+        """Appends an S3 Task definition to the stream and logs the sequence number.
+
+        Args:
+            file (str): The path of the file to be appended.
+        """
 
         # Prepare the S3 Task definition.
         _, tail = ntpath.split(file)
@@ -193,7 +226,12 @@ class DirectoryUploader:
         )
 
     async def _process_status(self, under_test: bool=False):
-        """Read the statuses from the export status stream."""
+        """Reads the statuses from the export status stream.
+
+        Args:
+            under_test (bool, optional): Flag to determine if the function is 
+                being executed under a test environment. Defaults to False.
+        """
 
         next_seq = 0
         keep_looping = True
@@ -209,7 +247,7 @@ class DirectoryUploader:
                         min_message_count=1,
                         max_message_count=5,
                         read_timeout_millis=1000,
-                    ),
+                    )
                 )
 
                 # Process each message.
@@ -224,14 +262,27 @@ class DirectoryUploader:
             except NotEnoughMessagesException:
                 # Ignore this exception, as it doesn't indicate an error.
                 pass
-            except Exception:
-                self.logger.exception("Exception while processing status")
+            except StreamManagerException as e:
+                self.logger.error(f"StreamManagerException occurred while processing status: {e}")
+            except ConnectionError:
+                self.logger.error("Connection error while processing status. Retrying in a few seconds...")
+                await asyncio.sleep(5)
+            except asyncio.TimeoutError:
+                self.logger.warning("Processing status request to StreamManager timed out. Retrying...")
+                await asyncio.sleep(1)
+            except Exception as e:
+                self.logger.exception(f"Unexpected error while processing status: {e}")
+                await asyncio.sleep(5)
             self.logger.debug(f"Sleeping for {self.status_interval} seconds")
             await asyncio.sleep(self.status_interval)
             keep_looping = not under_test
 
     def _handle_status_message(self, status_message: StatusMessage):
-        """Handle a status message."""
+        """Handles a status message based on its contents.
+
+        Args:
+            status_message (StatusMessage): The status message to be processed.
+        """
 
         file_url = status_message.status_context.s3_export_task_definition.input_url
         bucket = status_message.status_context.s3_export_task_definition.bucket
@@ -253,7 +304,7 @@ class DirectoryUploader:
             self.files_processed.remove(file_url.partition("file://")[2])
 
     async def run(self):
-        """Run the DirectoryUploader."""
+        """Starts the DirectoryUploader to monitor and upload files."""
 
         tasks = [
             asyncio.create_task(self._scan()),
@@ -262,6 +313,6 @@ class DirectoryUploader:
         await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
 
     def close(self):
-        """Close the DirectoryUploader."""
+        """Closes the DirectoryUploader and any associated resources."""
 
         self.client.close()
